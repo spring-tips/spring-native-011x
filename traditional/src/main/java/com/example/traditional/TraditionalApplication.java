35afba206e9e158fdd36bb18603a6560af0122b8
Josh Long <josh@joshlong.com>
2021-12-07 15:36:16 -0800

    up

--- traditional/src/main/java/com/example/traditional/TraditionalApplication.java
+++ traditional/src/main/java/com/example/traditional/TraditionalApplication.java
@@ -3,6 +3,7 @@ package com.example.traditional;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,11 +19,20 @@ import javax.persistence.Id;
 import java.util.Collection;
 import java.util.List;
 
+@Log4j2
 @SpringBootApplication
 public class TraditionalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TraditionalApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner runner(CustomerRepository repository) {
+		return args -> List.of("Andy", "Madhura", "Jürgen", "Yuxin", "Olga", "Violetta", "Stéphane", "Dr Syer")
+			.stream().map(name -> new Customer(null, name))
+			.map(repository::save)
+			.forEach(c -> log.info("customer: " + c));
 	}
 }
 
--- traditional/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into customer(id, name) values ( nextval('hibernate_sequence') , 'Jürgen' ) ;
-insert into customer(id, name) values ( nextval('hibernate_sequence') , 'Mike' ) ;
-insert into customer(id, name) values ( nextval('hibernate_sequence') , 'Madhura' ) ;
-insert into customer(id, name) values ( nextval('hibernate_sequence') , 'Yuxin' ) ;
-insert into customer(id, name) values ( nextval('hibernate_sequence') , 'Olga' ) ;
-insert into customer(id, name) values ( nextval('hibernate_sequence') , 'Violetta' ) ;
-insert into customer(id, name) values ( nextval('hibernate_sequence') , 'Stéphane' ) ;
-insert into customer(id, name) values ( nextval('hibernate_sequence') , 'Josh' ) ;
