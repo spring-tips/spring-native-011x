2207cea9539f73156001c8a2aa8c4fb972c36a2e
Josh Long <josh@joshlong.com>
2021-12-07 17:40:22 -0800

    spring factories

--- extensible/src/main/java/com/example/extensible/ExtensibleBeanFactoryNativeConfigurationProcessor.java
+++ extensible/src/main/java/com/example/extensible/ExtensibleBeanFactoryNativeConfigurationProcessor.java
@@ -5,22 +5,20 @@ import org.springframework.aot.context.bootstrap.generator.infrastructure.native
 import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.NativeConfigurationRegistry;
 import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.NativeProxyEntry;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.nativex.hint.JdkProxyHint;
 
-/*@JdkProxyHint(types = {
-	com.example.extensible.CustomerService.class,
-	org.springframework.aop.SpringProxy.class,
-	org.springframework.aop.framework.Advised.class,
-	org.springframework.core.DecoratingProxy.class
-})*/
+
 @Log4j2
 public class ExtensibleBeanFactoryNativeConfigurationProcessor
 	implements BeanFactoryNativeConfigurationProcessor {
+
+	private final boolean registerProxy = false;
 
 	@Override
 	public void process(ConfigurableListableBeanFactory beanFactory, NativeConfigurationRegistry registry) {
 		log.info(BeanFactoryNativeConfigurationProcessor.class);
 		var beans = beanFactory.getBeansOfType(CustomerService.class);
-		if (beans.size() > 0) {
+		if (registerProxy && beans.size() > 0) {
 			registry.proxy().add(NativeProxyEntry.ofInterfaces(CustomerService.class,
 				org.springframework.aop.SpringProxy.class,
 				org.springframework.aop.framework.Advised.class,
--- extensible/src/main/java/com/example/extensible/ExtensibleNativeConfiguration.java
+++ extensible/src/main/java/com/example/extensible/ExtensibleNativeConfiguration.java
@@ -3,11 +3,17 @@ package com.example.extensible;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.NativeConfigurationRegistry;
 import org.springframework.nativex.AotOptions;
+import org.springframework.nativex.hint.JdkProxyHint;
 import org.springframework.nativex.type.NativeConfiguration;
 
-
+@JdkProxyHint(types = {
+	com.example.extensible.CustomerService.class,
+	org.springframework.aop.SpringProxy.class,
+	org.springframework.aop.framework.Advised.class,
+	org.springframework.core.DecoratingProxy.class
+})
 @Log4j2
-public class ExtensibleTypeSystemNativeConfiguration implements NativeConfiguration {
+public class ExtensibleNativeConfiguration implements NativeConfiguration {
 
 	@Override
 	public boolean isValid(AotOptions aotOptions) {
