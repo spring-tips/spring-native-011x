043edcca5a1bd99919ff451aca7a6cb2143aec81
Josh Long <josh@joshlong.com>
2021-12-08 01:28:33 -0800

    Add files via upload
    i had a sample database. here's the schema defintion.

---
+++ data-and-schema.sql
@@ -0,0 +1,9 @@
+create table customer(id serial primary key, name varchar(255) not null ); 
+insert into customer(name) values ('Jürgen');
+insert into customer(name) values ('Stéphane');
+insert into customer(name) values ('Dr. Syer');
+insert into customer(name) values ('Yuxin');
+insert into customer(name) values ('Madhura');
+insert into customer(name) values ('Violetta');
+insert into customer(name) values ('Olga');
+insert into customer(name) values ('Josh');
